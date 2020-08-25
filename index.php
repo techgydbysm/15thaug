a643e288d448e26d255e0b5a5aac7ed8acc48ed8
Sukalyan Mukharjee <50608472+techgydbysm@users.noreply.github.com>
2020-08-25 09:39:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,186 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="canonical" href="index.html" itemprop="url" /> <title> Happy Independence Day 2018</title>
+<meta name="description" content="Create Happy Independence Day Wishes, 15 August 2018, Happy Independence Day 2018 wishing website" />
+<link rel="icon" href="https://4.bp.blogspot.com/-6xCkfaUUAew/W1gct0JohpI/AAAAAAAABbQ/Y7FuPrRg_s8dc4UuPelbpKxHNcRf-_tFwCLcBGAs/s1600/flag.jpg" sizes="32x32">
+<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=5b486eaae1ceeb001b842d49&product=sticky-share-buttons' async='async'></script>
+
+<meta property="og:title" content=" Wishing You Happy Independence Day 2018" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="index.php" />
+<meta property="og:image" content="happy-independence-day-15-august.png" />
+<meta property="og:image:width" content="640" />
+<meta property="og:image:height" content="415" />
+<meta property="og:description" content="From TechnoVedant<?php
+
+$n=$_GET['by'];
+
+echo "$n"
+
+?> 
+                                         
+Happy Independence Day Wishes, 15 August 2018, Make Happy Independence Day wish greetings card online #TechnoVedant" />
+
+
+<meta name="twitter:description" content="Create Happy Independence Day Wishes 2018, 15 August 2018, Make Happy Independence Day wish greetings card online" />
+<meta name="twitter:image" content="https://2.bp.blogspot.com/-vdjmfpaVRvc/W1gfkky7jWI/AAAAAAAABbo/-H0Af3bR9XgdgITgpc0qtlCBg81eKh4RQCLcBGAs/s1600/happy-independence-day-15-august.png" />
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css ">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+
+<body>
+  
+  <center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 300X50 Wishing Script -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:50px"
+     data-ad-client="ca-pub-3743485334306324"
+     data-ad-slot="3196563739"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+
+  
+    
+<div id="confetti"></div>
+<div class="container my
+            
+            
+            ">
+<div class="row">
+<div class="col-xs-12">
+
+</div>
+</div>
+</div>
+<h1 style="display:none;"> Happy Independence Day Script </h1>
+<div class="container">
+<div class="row">
+<div class="col-xs-12">
+<div class="msg-holder">
+<div class="msg">
+<?php
+
+$n=$_GET['by'];
+
+echo "$n"
+
+?> </div>
+</div>
+<div class="msg2"> Wishing You </div>
+<div class="msg3"> Happy Independence Day </div>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12">
+<h6 class="myimg"> <img src="https://1.bp.blogspot.com/-ph1Tl69E0aQ/W1ggs5HAw2I/AAAAAAAABb0/i5cuGX7cmucrspAqALK-vysex0ktsXpDgCLcBGAs/s1600/india-independence-day.png" class='img-responsive' alt="Happy Independence 2018"> </h6>
+</div>
+</div>
+</div>
+
+    
+  
+  
+  <div align="center"><div align="center"><script language="JavaScript" src="http://clickmasters.mobie.in/Jss/effect/agve-loverain.red.js"></script></div></div>  <div align="center"><div id="mainDiv">	<marquee scrollamount="3" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0"> <span style="background:	#FF0000;padding: 1px 7px;font-size: large;font-weight: bold;color: #FFF8DC;margin: 0px 0px;border-radius: 40px;"><font color="#F0FFFF">Feel the pride of being the part of such a glorious nation. Here’s sending my warm patriotic wishes to make this day truly memorable. </font></span><span style="background:#0000FF;padding: 1px 7px;font-size: large;font-weight: bold;color: #FF0000;margin: 0px 0px;border-radius: 40px;"><font color="white">Independence is a Precious gift of God. May We Always Remain Independent A Very Happy Independence Day To You.</font></span><span style="background:	Pink;padding: 1px 7px;font-size: large;font-weight: bold;color: #00FF00;margin: 0px 0px;border-radius: 40px;"><font color="#000000">Happy Indian Independence Day 2018</font></span></marquee></div></div>
+    
+    
+    
+<div class="container myads">
+<div class="row">
+<div class="col-xs-12">
+</div>
+</div>
+</div>
+<div class="container-fluid" id="box">
+<div class="row box2">
+<div class="col-xs-12 col-md-6">
+<form method="get" class="form-inline myform">
+<div class="form-group">
+<input type="text" class="form-control mytext" name="by" placeholder="Enter Your Name" required>
+</div>
+    <center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 300X50 Wishing Script -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:50px"
+     data-ad-client="ca-pub-3743485334306324"
+     data-ad-slot="3196563739"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+<button type="submit" class="btn btn-default mybtn">Create Wish</button>
+</form>
+</div>
+  
+  
+<div class="col-xs-12 col-md-6">
+<div class="tt">
+<span class="sb-title"> <i class="fa fa-share-alt" aria-hidden="true"></i> Share With Your Friends </span>
+<span class="sb-share">
+<div class="wp sb-icon"> <a href="whatsapp://send?text= From <?php
+
+$n=$_GET['by'];
+
+echo "*$n*"
+
+?>
+&#10;
+
+%20Created%20Something%20Special%20Only%20For%20You%20Touch%20The%20Blue%20Line <?php echo $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>" target="_blank" title="Share this post on Whatsapp" class="whatsapp"> <i class="fa fa-whatsapp" aria-hidden="true"></i> Whatsapp </a> </div>
+
+
+
+  
+</span>
+</div>
+</div>
+</div>
+</div>
+<div class="container myads">
+<div class="row">
+<div class="col-xs-12">
+</div>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="/js/bird.js"></script>
+
+    
+      <center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 300X50 Wishing Script -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:50px"
+     data-ad-client="ca-pub-3743485334306324"
+     data-ad-slot="3196563739"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+    
+
+<script src="TechnoVedant.js"></script>
+<script type="text/javascript" src="doodle.js"></script>
+<script type="text/javascript">
+		doodle.init("jet.png");
+	</script>
+	
+	<?php
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>
+ 
+</body>
+</html>
